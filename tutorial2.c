126b4c64e7e15d3727bc8bf8a8743c84c6083f66
DummiesAlert <george298829@gmail.com>
2026-05-12 14:59:35 -0600

    commit

--- tutorial2.c
+++ tutorial2.c
@@ -1,28 +1,67 @@
-// int number = 0;
-// int total = 0; 
+// Factorial Program
+
+// #include <stdio.h> 
 
 // int main() {
-//     printf("Enter a number: ");
+//   int n;
+//   int factorial = 1;
+//   scanf("%d", &n);
 
-//     for (int i = 0; i = number; i++) {
-//         total = total * number
-//         number--;
-//     }
+//   for (int i = 1; i <= 0; i++) {
+//     factorial = factorial * i; 
+//   }
 
-//     printf("The factorial of %d\n !", number, "is" total);
+// printf("%d\n", factorial);
+
+// return 0;
 // }
 
-#include <stdio.h> 
+// Take a string as input, traverse backwards, and print every character
+// you can use string.h strlen(s)
 
-int main() {
-  int n;
-  int factorial = 1;
-  scanf("%d", &n);
+// #include <stdio.h> 
+// #include <string.h> 
 
-  for (int i = 1; i <= 0; i++) {
+// int main() {
+//   char word[100];
+//   scanf("%s", word);
 
-    factorial = factorial * i}
+//   int len = strlen(word); 
 
-printf("%\n", factorial);
-return 0; 
-  
+//   for(int i = strlen(word) - 1; i >= 0; i--) { 
+//     printf("%c", word[i]);
+//   }
+
+//   printf("\n");
+//   return 0; 
+// }
+
+// Pointers Example
+
+// int main() {
+//   char a = "a";
+//   char *p = &a; 
+
+//   *p = "b";
+//   printf("%c", a);
+
+//   return 0;
+// }
+
+// Pointer Exercise: Write a functions to swap two numbers using pointers
+
+void swap(char *p1, char *p2) {
+  char temp = *p1;
+  *p1 = *p2;
+  *p2 = temp; 
+
+  printf("%c", p1);
+  printf("%c", p2);
+}
+
+int main() { 
+  char p1p = "me";
+  char p2p = "you";
+
+  swap(p1p, p2p);
+}
