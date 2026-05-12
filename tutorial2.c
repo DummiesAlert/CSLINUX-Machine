d62fccd5e130f08686d5821eacee8dc835509809
Zhuo Xi Hong <100309969+DummiesAlert@users.noreply.github.com>
2026-05-12 14:29:43 -0600

    factorial code

---
+++ tutorial2.c
@@ -0,0 +1,28 @@
+// int number = 0;
+// int total = 0; 
+
+// int main() {
+//     printf("Enter a number: ");
+
+//     for (int i = 0; i = number; i++) {
+//         total = total * number
+//         number--;
+//     }
+
+//     printf("The factorial of %d\n !", number, "is" total);
+// }
+
+#include <stdio.h> 
+
+int main() {
+  int n;
+  int factorial = 1;
+  scanf("%d", &n);
+
+  for (int i = 1; i <= 0; i++) {
+
+    factorial = factorial * i}
+
+printf("%\n", factorial);
+return 0; 
+  
