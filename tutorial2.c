2b25a18f19d712234efcbc4eccc9ad5865c1a417
30213715 <zhuoxi.hong1@ucalgary.ca>
2026-05-12 15:01:58 -0600

    yes

--- tutorial2.c
+++ tutorial2.c
@@ -50,18 +50,22 @@
 
 // Pointer Exercise: Write a functions to swap two numbers using pointers
 
-void swap(char *p1, char *p2) {
-  char temp = *p1;
+#include <stdio.h>
+
+void swap(char **p1, char **p2) {
+  char *temp = *p1;
   *p1 = *p2;
   *p2 = temp; 
 
-  printf("%c", p1);
-  printf("%c", p2);
+  printf("%s\n", *p1);
+  printf("%s\n", *p2);
 }
 
 int main() { 
-  char p1p = "me";
-  char p2p = "you";
+  char *p1p = "me";
+  char *p2p = "you";
 
-  swap(p1p, p2p);
+  swap(&p1p, &p2p);
+
+  return 0;
 }
