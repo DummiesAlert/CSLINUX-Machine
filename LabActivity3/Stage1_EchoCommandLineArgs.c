fdf0c76f425b9438d84b0f5fa7024d1def7b7ed5
30213715 <zhuoxi.hong1@ucalgary.ca>
2026-05-21 14:18:00 -0600

    Stage2 Test

--- LabActivity3/Stage1_EchoCommandLineArgs.c
+++ LabActivity3/Stage1_EchoCommandLineArgs.c
@@ -4,8 +4,21 @@
 
 int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
-        printf("The Argument %d: %s\n", i, argv[i]);
+        printf("Argument %d: %s\n", i, argv[i]);
     }
 
     return 0;
 }
+
+/*
+
+Output:
+
+gcc Stage1_EchoCommandLineArgs.c -o 1.out
+./1.out banana and cookie
+
+Argument 1: banana
+Argument 2: and
+Argument 3: cookie
+
+*/
--- LabActivity3/Stage2_EchoCommandLineArgsWithVowel.c
+++ LabActivity3/Stage2_EchoCommandLineArgsWithVowel.c
@@ -1,1 +1,28 @@
 // Write a C program which echoes all command line arguments which begin with a vowel
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++) {
+        char firstChar = argv[i][0];
+        if (firstChar == 'A' || firstChar == 'E' || firstChar == 'I' || firstChar == 'O' || firstChar == 'U' ||
+            firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u') {
+            printf("Argument %d: %s\n", i, argv[i]);
+        }
+    }
+
+    return 0;
+}
+
+/*
+
+Output:
+
+gcc Stage1_EchoCommandLineArgs.c -o 1.out
+./2.out banana and cookie
+
+Argument 1: banana
+Argument 2: and
+Argument 3: cookie
+
+*/
