8fac6b2e236ceff5f71ddbbce89e086d0b642b5d
DummiesAlert <george298829@gmail.com>
2026-05-21 15:38:22 -0600

    DONE!!!!!

--- LabActivity3/Stage4_RepeatStage3AndUsegetOpt.c
+++ LabActivity3/Stage4_RepeatStage3AndUsegetOpt.c
@@ -5,7 +5,6 @@
 
 int main(int argc, char *argv[]) {
     int opt;
-
     while ((opt = getopt(argc, argv, "abc")) != -1) {
 
         if (opt != '?') {
@@ -30,10 +29,13 @@ gcc Stage4_FlagsAndCommandLineArguments.c -o 4.out
 ./4.out -a banana -ab and --b cookie
 
 Flag set: -a
-Argument 2: banana
-Flag set; -ab
-Argument 4: and
-Argument 5: --b
+Flag set: -a
+Flag set: -b
+./4.out: invalid option -- '-'
+Unknown/Error set: --
+Flag set: -b
+Argument 4: banana
+Argument 5: and
 Argument 6: cookie
 
 */
