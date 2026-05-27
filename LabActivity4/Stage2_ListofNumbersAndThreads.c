78a3fcc996b5173b1473a3a500f4cdf493e37653
30213715 <zhuoxi.hong1@ucalgary.ca>
2026-05-26 20:13:12 -0600

    ;

--- LabActivity4/Stage2_ListofNumbersAndThreads.c
+++ LabActivity4/Stage2_ListofNumbersAndThreads.c
@@ -18,7 +18,7 @@ void *countdown(void *arg) {
 
     for (int i = number->numbers; i >= 0; i--) {
         printf("Thread %d: %d\n", number->threadNumber, i);
-        sleep(2)
+        sleep(2);
     }
 
     return NULL;
